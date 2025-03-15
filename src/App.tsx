ebf21dc0a944354226b1e26ee52a6ebc3812f2ed
namanchandak <98888986+namanchandak@users.noreply.github.com>
2025-03-15 10:38:02 +0530

    login and signup fe

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Login from './components/login';
+import Signup from './components/signup';
+import './App.css';
 
-
-const App = () => {
-
-  const [login, setLogin] = useState(1);
+const App: React.FC = () => {
+  const [login, setLogin] = useState<boolean>(true);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline ">
-
-        Welcome to Image App
-      </h1>
-
-      <div className="flex flex-wrap my-10 mx-10">
-
-
-        {login === 0 ?
-
-          <div>
-
-
-            <button onClick={() => { setLogin(1) }} type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
-          </div>
-          :
-          <div>
-            <button onClick={() => { setLogin(0) }} type="button" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Signup</button>
-
-          </div>
-
-        }
-
-
-
+    <div className="app-container">
+      <h1 className="app-title">Welcome to Image App</h1>
+      <div className="auth-container">
+        {login ? (
+          <Login setLogin={setLogin} />
+        ) : (
+          <Signup setLogin={setLogin} />
+        )}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
